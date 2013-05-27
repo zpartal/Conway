f9ef29a79446c9b3f576ca8b09906f6cb5948842
Zachary Partal <zpartal@gmail.com>
2013-05-27 12:01:13 -0500

    moved the board into it's own class

--- conway.py
+++ conway.py
@@ -5,8 +5,8 @@ import pygame, random, sys, math
 from pygame.locals import *
 
 CELLSIZE = 10 # size of cells in pixels
-GRIDWIDTH = 64
-GRIDHEIGHT = 48
+GRIDWIDTH = 5
+GRIDHEIGHT = 5
 window_width = GRIDWIDTH * CELLSIZE
 window_height = GRIDHEIGHT * CELLSIZE
 FPS = 30
@@ -19,27 +19,84 @@ GREEN     = (  0, 255,   0)
 BLUE      = (  0,   0, 255)
 BGCOLOR = WHITE
 
-
-class cell:
+class Cell:
+    pos_x = 0 # horizontal cell grid position
+    pos_y = 0 # vertical cell grid position
+    state = 0 # 1 for alive, 0 for dead
     def __init__(self, x, y):
         self.pos_x = x # horizontal cell grid position
         self.pos_y = y # vertical cell grid position
-        self.state = 0 # 1 for alive, 0 for dead
     def print_loc(self):
-        print(str(self.pos_x) + "," + str(self.pos_y))
+        print(str(pos_x) + "," + str(pos_y))
     def kill(self):
         self.state = 0
     def live(self):
         self.state = 1
+    def isAlive(self):
+        if self.state == 1:
+            return True
+        else: 
+            return False
+
+class CellBoard:
+    cell_table = []
+    next_state = []
+    width = 0
+    height = 0
+    def __init__(self, _width, _height):
+        self.width = _width
+        self.height = _height
+        for x in range(self.width):
+            new_column = []
+            for y in range(self.height):
+                new_column.append(Cell(x,y))
+            self.cell_table.append(new_column)
+            self.next_state.append(new_column)
+
+    def clear_board(self):
+        for x in range(self.width):
+            for y in range(self.height):
+                self.cell_table[x][y].kill()
+
+    def num_neighbors(self, _x, _y):
+        neighbor_count = 0
+        for x in range(-1,2):
+            for y in range(-1,2):
+                # print("[" + str(_x+x) + "," + str(_y+y) + "]")
+                if x == 0 and y == 0: pass
+                elif _x+x >= self.width or _y+y >= self.height: pass
+                elif _x+x < 0 or _y+y < 0: pass
+                else:
+                    # print("[" + str(_x+x) + "," + str(_y+y) + "]")
+                    if self.cell_table[_x+x][_y+y].isAlive():                    
+                        neighbor_count += 1
+        # print("neighbor count: " + str(neighbor_count))
+        return neighbor_count
+
+    def update_cell_state(self, _x, _y):
+        live_neighbors = num_neighbors(_x, _y)
+        # print("Cell: [" + str(_x) + "," + str(_y) + "] Neighbors: " + str(live_neighbors))
+        if live_neighbors < 2: self.next_state[_x][_y].kill()
+        elif live_neighbors >= 2 and live_neighbors <= 3: self.next_state[_x][_y].live()
+        elif live_neighbors > 3: self.next_state[_x][_y].kill()
+
+    def generate_next_state(self):
+        for x in range(self.width):
+            for y in range(self.height):
+                update_cell_state(x, y)
+        self.cell_table = self.next_state
+
+    def cell(self, _x, _y):
+        return self.cell_table[_x][_y]
 
 def main():
-    global FPSCLOCK, DISPLAYSURF, BASICFONT, cell_table, running_state
+    global FPSCLOCK, DISPLAYSURF, BASICFONT, game_board, running_state
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((window_width, window_height),0,32)
     BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
     pygame.display.set_caption('Conway')
-    cell_table = generateCells()
+    game_board = CellBoard(GRIDWIDTH, GRIDHEIGHT)
     running_state = 0 # 0 if game is not running, 1 if game is running
     runGame()
 
@@ -72,76 +129,31 @@ def runGame():
                         print("Not Running")
                 elif event.key == K_c:
                     if running_state == 0:
-                        reset_cells()
+                        game_board.clear_board()
 
         DISPLAYSURF.fill(BGCOLOR)
         if running_state == 1:
-            generateCells()
+            pass
+            # game_board.generate_next_state()
         drawGrid()
-        drawCells(cell_table)
+        drawCells()
         pygame.display.update()
         FPSCLOCK.tick(FPS)
 
-def generateCells():
-    cell_table = []
-    for x in range(GRIDWIDTH):
-        new_column = []
-        for y in range(GRIDHEIGHT):
-            new_column.append(cell(x,y))
-        cell_table.append(new_column)
-    return cell_table
-
-def generate_state():
-    # Any live cell with fewer than two live neighbors dies, as if caused by under-population.
-    # Any live cell with two or three live neighbors lives on to the next generation.
-    # Any live cell with more than three live neighbors dies, as if by overcrowding.
-    # Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-    new_state = cell_table
-    for x in range(GRIDWIDTH):
-        for y in range(GRIDHEIGHT):
-            update_cell_state(new_state, cell_table, x, y)
-    cell_table = new_state
-
-def num_neighbors(current_state, _x, _y):
-    neighbor_count = 0
-    for x in range(-1,2):
-        for y in range(-1,2):
-            # print("[" + str(_x+x) + "," + str(_y+y) + "]")
-            if x == 0 and y == 0: pass
-            elif _x+x >= GRIDWIDTH or _y+y >= GRIDHEIGHT: pass
-            elif _x+x < 0 or _y+y < 0: pass
-            else:
-                # print("[" + str(_x+x) + "," + str(_y+y) + "]")
-                if cell_table[_x+x][_y+y].state == 1:                    
-                    neighbor_count += 1
-    # print("neighbor count: " + str(neighbor_count))
-    return neighbor_count
-
-def update_cell_state (new_state, current_state, _x, _y):
-    live_neighbors = num_neighbors(current_state, _x, _y)
-    if live_neighbors < 2: new_state[_x][_y].kill()
-    elif live_neighbors >= 2 and live_neighbors <= 3: new_state[_x][_y].live()
-    elif live_neighbors > 3: new_state[_x][_y].kill()
 
 def click_cell(_mouse_x, _mouse_y):
     gridx = math.floor( _mouse_x / CELLSIZE )
     gridy = math.floor( _mouse_y / CELLSIZE )
-    print("Clicked: [" + str(gridx) + "," + str(gridy) + "]")
-    # print("Clicked: [" + str(_mouse_x) + "," + str(_mouse_y) + "], [" + str(gridx) + "," + str(gridy) + "]")
-    if cell_table[gridx][gridy].state == 1:
-        cell_table[gridx][gridy].kill()
+    if game_board.cell(gridx,gridy).isAlive():
+        game_board.cell(gridx,gridy).kill()
     else:
-        cell_table[gridx][gridy].live()
+        game_board.cell(gridx,gridy).live()
+    print("Clicked: [" + str(gridx) + "," + str(gridy) + "], state: " + str(game_board.cell_table[gridx][gridy].state))
 
 def print_neighbor_count(_mouse_x, _mouse_y):
     gridx = math.floor( _mouse_x / CELLSIZE )
     gridy = math.floor( _mouse_y / CELLSIZE )
-    print(str(num_neighbors(cell_table, gridx,gridy)))
-
-def reset_cells():
-    for x in range(GRIDWIDTH):
-        for y in range(GRIDHEIGHT):
-            cell_table[x][y].kill()
+    print(str(game_board.num_neighbors(gridx,gridy)))
 
 def drawGrid():
     for x in range(0, window_width, CELLSIZE): # draw vertical lines
@@ -149,10 +161,10 @@ def drawGrid():
     for y in range(0, window_height, CELLSIZE): # draw horizontal lines
         pygame.draw.line(DISPLAYSURF, BLACK, (0, y), (window_width, y))
 
-def drawCells(_cell_table):
+def drawCells():
     for x in range(GRIDWIDTH):
         for y in range(GRIDHEIGHT):
-            if _cell_table[x][y].state == 1:
+            if game_board.cell(x,y).isAlive():
                 rect_coords = pygame.Rect(x*CELLSIZE, y*CELLSIZE, CELLSIZE, CELLSIZE)
                 pygame.draw.rect(DISPLAYSURF, BLACK, rect_coords)
 
